bfcf7ee0a21ac6e5e81ebc8e4d2d6c149a1aa028
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-18 11:30:46 -0500

    Individual User Edit page works

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -18,30 +18,41 @@ router.get('/new', (req, res) => {
 })
 /*************** CREATE ROUTE ***************/
 router.post('/', (req, res) => {
-	Users.create(req.body, req.params.id, (err, createdUser) => {
-		res.redirect('/users');	
-		console.log(createdUser);
+	Users.create(req.body, req.params.id,
+	 (err, createdUser) => {
+	 	if(err){console.log('------------ERROR----------', err);
+	 }else{console.log('-------------CREATED USER-------------', createdUser);
+	}
+			// console.log(createdUser);
 		})
+		res.redirect('/users');	
 	});
 /*************** SHOW ROUTE ***************/
 router.get('/:id', (req, res) => {
 	Users.findById(req.params.id,
 		(err, foundUser) => {
 			if(err){console.log('------------ERROR----------', err);
+		}else{console.log('-------------FOUND USER-------------', foundUser);
+			res.render('../views/users/show.ejs',{users:foundUser});		
+		}	
 		}
-				else{console.log('-------------FOUND USER-------------', foundUser);
-					res.render('../views/users/show.ejs',
-					{
-						users:foundUser
-					});
-				
-			}	
+	)
+})
+/*************** EDIT ROUTE ***************/
+router.get('/:id/edit', (req, res) => {
+	Users.findById(req.params.id,
+		(err, editUser) => {
+			console.log(editUser);
+			if (err){console.log(`---------- ERROR ---------- \n`, err);}
+				else {
+				console.log(`---------- EDIT USER ---------- \n`, editUser);
+				res.render('../views/users/edit.ejs', {user:editUser});
+			}
 		}
 	)
 })
 /*************** DESTROY ROUTE ***************/
 
-/*************** EDIT ROUTE ***************/
 
 /*************** UPDATE ROUTE ***************/
 
